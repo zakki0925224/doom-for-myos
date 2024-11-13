c3b6094b8a4be6a3ade39d92990efa5b093f1db1
Zakki0925224 <zakki0925224@gmail.com>
2024-11-13 22:43:49 +0900

    [Add]: Added `init_ticks_ms` to `doomgeneric_myos.c`

--- doomgeneric_myos.c
+++ doomgeneric_myos.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <syscalls.h>
 
 #include "doomgeneric.h"
 
+static uint32_t init_ticks_ms = 0;
+
 void DG_Init()
 {
+    init_ticks_ms = (uint32_t)sys_uptime();
 }
 
 void DG_DrawFrame()
 {
+    printf("%d\n", ((char *)DG_ScreenBuffer)[0]);
 }
 
 void DG_SleepMs(uint32_t ms)
@@ -16,7 +21,7 @@ void DG_SleepMs(uint32_t ms)
 
 uint32_t DG_GetTicksMs()
 {
-    return 0;
+    return (uint32_t)sys_uptime() - init_ticks_ms;
 }
 
 int DG_GetKey(int *pressed, unsigned char *doomKey)
