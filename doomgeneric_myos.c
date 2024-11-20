5041d9bdd488507e491be88a491834ec1a55ad95
Zakki0925224 <zakki0925224@gmail.com>
2024-11-20 22:47:18 +0900

    [Add]: Added input key reader

--- doomgeneric_myos.c
+++ doomgeneric_myos.c
@@ -1,22 +1,94 @@
+#include "doomgeneric.h"
+#include "doomkeys.h"
+
 #include <stdio.h>
 #include <syscalls.h>
-
-#include "doomgeneric.h"
+#include <window.h>
+#include <ctype.h>
 
 static uint32_t init_ticks_ms = 0;
+static WindowDescriptor *wdesc = NULL;
+static char input_key;
+
+#define KEYQUEUE_SIZE 16
+
+static unsigned short s_keyQueue[KEYQUEUE_SIZE];
+static unsigned int s_KeyQueueWriteIndex = 0;
+static unsigned int s_KeyQueueReadIndex = 0;
+
+static unsigned char convertToDoomKey(char key)
+{
+    key = tolower(key);
+
+    switch (key)
+    {
+    case '\n':
+        return KEY_ENTER;
+    case 'e':
+        return KEY_ESCAPE;
+    case 'a':
+        return KEY_LEFTARROW;
+    case 'd':
+        return KEY_RIGHTARROW;
+    case 'w':
+        return KEY_UPARROW;
+    case 's':
+        return KEY_DOWNARROW;
+    case ' ':
+        return KEY_USE;
+    case 'i':
+        return KEY_FIRE;
+    case 'o':
+        return KEY_RSHIFT;
+    default:
+        return key;
+    }
+}
+
+static void addKeyToQueue(int pressed, char key)
+{
+    unsigned short keyData = (pressed << 8) | convertToDoomKey(key);
+
+    s_keyQueue[s_KeyQueueWriteIndex] = keyData;
+    s_KeyQueueWriteIndex++;
+    s_KeyQueueWriteIndex %= KEYQUEUE_SIZE;
+}
 
 void DG_Init()
 {
+    // tick
     init_ticks_ms = (uint32_t)sys_uptime();
+
+    // window
+    wdesc = create_window("DOOM", 0, 0, 320, 200);
+    if (wdesc == NULL)
+    {
+        printf("Failed to create window\n");
+        return;
+    }
 }
 
 void DG_DrawFrame()
 {
-    printf("%d\n", ((char *)DG_ScreenBuffer)[0]);
+    // printf("%d\n", ((char *)DG_ScreenBuffer)[0]);
+    if (sys_read(FDN_STDIN, &input_key, 1) == -1)
+        return;
+
+    if (input_key == '\0')
+        return;
+
+    addKeyToQueue(1, input_key);
+    addKeyToQueue(0, input_key);
 }
 
 void DG_SleepMs(uint32_t ms)
 {
+    uint32_t start = DG_GetTicksMs();
+    for (;;)
+    {
+        if (DG_GetTicksMs() - start >= ms)
+            break;
+    }
 }
 
 uint32_t DG_GetTicksMs()
@@ -26,7 +98,17 @@ uint32_t DG_GetTicksMs()
 
 int DG_GetKey(int *pressed, unsigned char *doomKey)
 {
-    return 0;
+    if (s_KeyQueueReadIndex == s_KeyQueueWriteIndex)
+        return 0;
+
+    unsigned short keyData = s_keyQueue[s_KeyQueueReadIndex];
+    s_KeyQueueReadIndex++;
+    s_KeyQueueReadIndex %= KEYQUEUE_SIZE;
+
+    *pressed = keyData >> 8;
+    *doomKey = keyData & 0xff;
+
+    return 1;
 }
 
 void DG_SetWindowTitle(const char *title)
