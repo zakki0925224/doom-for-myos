56f59b1ca4330d6e1a4e7770e5fcffa8ee2a254c
zakki0925224 <zakki0925224@gmail.com>
2025-06-04 16:19:04 +0900

    [Add]: Added interrupt switch

--- doomgeneric_myos.c
+++ doomgeneric_myos.c
@@ -1,10 +1,10 @@
-#include "doomgeneric.h"
-#include "doomkeys.h"
-
+#include <ctype.h>
 #include <stdio.h>
 #include <syscalls.h>
 #include <window.h>
-#include <ctype.h>
+
+#include "doomgeneric.h"
+#include "doomkeys.h"
 
 static uint32_t init_ticks_ms = 0;
 static WindowDescriptor *wdesc = NULL;
@@ -17,37 +17,34 @@ static unsigned short s_keyQueue[KEYQUEUE_SIZE];
 static unsigned int s_KeyQueueWriteIndex = 0;
 static unsigned int s_KeyQueueReadIndex = 0;
 
-static unsigned char convertToDoomKey(char key)
-{
+static unsigned char convertToDoomKey(char key) {
     key = tolower(key);
 
-    switch (key)
-    {
-    case '\n':
-        return KEY_ENTER;
-    case 'e':
-        return KEY_ESCAPE;
-    case 'a':
-        return KEY_LEFTARROW;
-    case 'd':
-        return KEY_RIGHTARROW;
-    case 'w':
-        return KEY_UPARROW;
-    case 's':
-        return KEY_DOWNARROW;
-    case ' ':
-        return KEY_USE;
-    case 'i':
-        return KEY_FIRE;
-    case 'o':
-        return KEY_RSHIFT;
-    default:
-        return key;
+    switch (key) {
+        case '\n':
+            return KEY_ENTER;
+        case 'e':
+            return KEY_ESCAPE;
+        case 'a':
+            return KEY_LEFTARROW;
+        case 'd':
+            return KEY_RIGHTARROW;
+        case 'w':
+            return KEY_UPARROW;
+        case 's':
+            return KEY_DOWNARROW;
+        case ' ':
+            return KEY_USE;
+        case 'i':
+            return KEY_FIRE;
+        case 'o':
+            return KEY_RSHIFT;
+        default:
+            return key;
     }
 }
 
-static void addKeyToQueue(int pressed, char key)
-{
+static void addKeyToQueue(int pressed, char key) {
     unsigned short keyData = (pressed << 8) | convertToDoomKey(key);
 
     s_keyQueue[s_KeyQueueWriteIndex] = keyData;
@@ -55,33 +52,34 @@ static void addKeyToQueue(int pressed, char key)
     s_KeyQueueWriteIndex %= KEYQUEUE_SIZE;
 }
 
-void DG_Init()
-{
+void DG_Init() {
     // tick
     init_ticks_ms = (uint32_t)sys_uptime();
 
     // window
     wdesc = create_window("DOOM", 0, 0, DOOMGENERIC_RESX + 8, DOOMGENERIC_RESY + 30);
-    if (wdesc == NULL)
-    {
+    if (wdesc == NULL) {
         printf("Failed to create window\n");
         return;
     }
 
-    if (add_image_to_window(wdesc, DOOMGENERIC_RESX, DOOMGENERIC_RESY, PIXEL_FORMAT_BGRA, (char *)DG_ScreenBuffer) == -1)
-    {
+    if (add_image_to_window(wdesc, DOOMGENERIC_RESX, DOOMGENERIC_RESY, PIXEL_FORMAT_BGRA, (char *)DG_ScreenBuffer) == -1) {
         printf("Failed to add image to window\n");
         return;
     }
 }
 
-void DG_DrawFrame()
-{
+void DG_DrawFrame() {
     if (sys_read(FDN_STDIN, &input_key, 1) == -1)
         return;
 
     if (input_key == '\0')
         return;
+
+    if (input_key == 0x03) {
+        printf("Interrupt.\n");
+        exit(0);
+    }
 
     if (input_key != before_input_key && before_input_key != '\0')
         addKeyToQueue(0, before_input_key);
@@ -90,23 +88,19 @@ void DG_DrawFrame()
     before_input_key = input_key;
 }
 
-void DG_SleepMs(uint32_t ms)
-{
+void DG_SleepMs(uint32_t ms) {
     uint32_t start = DG_GetTicksMs();
-    for (;;)
-    {
+    for (;;) {
         if (DG_GetTicksMs() - start >= ms)
             break;
     }
 }
 
-uint32_t DG_GetTicksMs()
-{
+uint32_t DG_GetTicksMs() {
     return (uint32_t)sys_uptime() - init_ticks_ms;
 }
 
-int DG_GetKey(int *pressed, unsigned char *doomKey)
-{
+int DG_GetKey(int *pressed, unsigned char *doomKey) {
     if (s_KeyQueueReadIndex == s_KeyQueueWriteIndex)
         return 0;
 
@@ -120,12 +114,10 @@ int DG_GetKey(int *pressed, unsigned char *doomKey)
     return 1;
 }
 
-void DG_SetWindowTitle(const char *title)
-{
+void DG_SetWindowTitle(const char *title) {
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     doomgeneric_Create(argc, argv);
 
     while (1)
